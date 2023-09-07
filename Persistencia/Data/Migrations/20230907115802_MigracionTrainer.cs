b31bcb980789607fd7dee9bb1fef59a4edda8b43
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-09-07 08:36:19 -0500

    feat: :sparkles: Migration
    New Migration

---
+++ Persistencia/Data/Migrations/20230907115802_MigracionTrainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistencia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionTrainer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
